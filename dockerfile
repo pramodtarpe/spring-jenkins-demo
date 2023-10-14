0af355671f26e378d2d833c053e998917bd8d614
Pramod <pramodtarpe12@gmail.com>
2023-10-14 23:08:27 +0530

    docker java 17 added

--- dockerfile
+++ dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8
+FROM openjdk:17
 EXPOSE 8080
 ADD build/libs/spring-jenkins-demo.jar spring-jenkins-demo.jar
 ENTRYPOINT ["java","-jar","/spring-jenkins-demo.jar"]
--- src/main/java/com/jenkins/Application.java
+++ src/main/java/com/jenkins/Application.java
@@ -13,6 +13,7 @@ public class Application {
     public static void main(String[] args) {
 
         log.info("Application Started...");
+        log.info("Hello Docker");
         SpringApplication.run(Application.class, args);
     }
 
