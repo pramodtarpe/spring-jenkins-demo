4a811859edd671c41e9d2253b41c04c2b7599411
Pramod <pramodtarpe12@gmail.com>
2023-10-14 19:16:33 +0530

    test changes

--- src/test/java/com/jenkins/ApplicationTests.java
+++ src/test/java/com/jenkins/ApplicationTests.java
@@ -1,10 +1,14 @@
 package com.jenkins;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class ApplicationTests {
 
