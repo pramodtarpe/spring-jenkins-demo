9f59cccc3f67ee6f3a5c10729f8b33d9905f1525
Pramod <pramodtarpe12@gmail.com>
2023-10-14 20:43:37 +0530

    logger on test

--- src/test/java/com/jenkins/ApplicationTests.java
+++ src/test/java/com/jenkins/ApplicationTests.java
@@ -3,6 +3,8 @@ package com.jenkins;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,8 +14,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class ApplicationTests {
 
+    private static final Logger log = LoggerFactory.getLogger(ApplicationTests.class);
+
     @Test
     void contextLoads() {
+        log.info("HELLO PIPELINE !!!");
         assertTrue(true);
     }
 
